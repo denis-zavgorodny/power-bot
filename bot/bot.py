f4dae0c4fe03e6ebe5ed0c9d5dd069d6b5e9920b
denys.zavhorodnii <denis.zavgorodny@gmail.com>
2024-07-19 21:03:09 +0300

    update bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,13 +1,30 @@
 import telebot
 from telebot import types
+import requests
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 bot = telebot.TeleBot(config.get("BOT_TOKEN"))
+bot.delete_my_commands()
+
+
+GET_STATUS = "Що по світлу?"
+SUBSCRIBE = "Отримувати статуси"
+
+greating = """
+    Вітаю! 
+Я можу дізнаватись для вас чи є світло в домі.
+
+А ще я можу відправляти вам повідомлення коли світло зʼявляється чи зникає.
+"""
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Вітаю! Я знаю що у вас з електрикою в домі")
+    markup = types.ReplyKeyboardMarkup()
+    markup.row(types.KeyboardButton(GET_STATUS))
+    markup.row(types.KeyboardButton(SUBSCRIBE))
+
+    bot.reply_to(message, greating, reply_markup=markup)
 
 @bot.message_handler(commands=['options'])
 def send_options(message):
@@ -24,9 +41,23 @@ def handle_query(call):
     elif call.data == 'B':
         bot.answer_callback_query(call.id, "You chose Option B")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#     bot.reply_to(message, message.text)
+
+@bot.message_handler(func=lambda message: message.text == GET_STATUS)
+def get_status(message):
+    response = requests.get(config.get("GET_STATUS_ENDPOINT"))
+
+    if response.status_code == 200:
+        bot.reply_to(message, "Світло є!")
+    else:
+        bot.reply_to(message, "Світла немає :(")
+
+
+@bot.message_handler(func=lambda message: message.text == SUBSCRIBE)
+def get_status(message):
+    bot.reply_to(message, "Підписано")
 
 if __name__ == '__main__':
     # Start the bot
