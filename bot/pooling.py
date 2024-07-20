a802cdd485672e43ed0708aa1f2c7761f5570d09
denys.zavhorodnii <denis.zavgorodny@gmail.com>
2024-07-20 23:52:58 +0300

    add max backoff time

--- bot/pooling.py
+++ bot/pooling.py
@@ -2,13 +2,19 @@ import time
 
 import requests
 
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
 
 def poolingStatus(endpoint, callback, interval=60):
+    session = requests_session_with_retries(retries=3, backoff_factor=1)
     isElectricityAvailable = None
 
     while True:
         try:
-            response = requests.get(endpoint)
+            response = session.get(endpoint)
+            response.raise_for_status()
+
 
             if response.status_code == 200:
                 if isElectricityAvailable is False:
@@ -26,3 +32,23 @@ def poolingStatus(endpoint, callback, interval=60):
             print(f"Request failed: {e}")
 
         time.sleep(interval)
+
+
+def requests_session_with_retries(retries=3, backoff_factor=0.3, status_forcelist=(500, 502, 504, 404)):
+    session = requests.Session()
+
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+        backoff_max=10
+    )
+
+    adapter = HTTPAdapter(max_retries=retry)
+
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+
+    return session
