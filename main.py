cac05a2d7193de8e4b7cdc1f007b6295c6375f9e
denys.zavhorodnii <denis.zavgorodny@gmail.com>
2024-07-21 18:35:14 +0300

    fix time filters

--- main.py
+++ main.py
@@ -69,34 +69,49 @@ def ping():
 @app.route("/stat")
 @get_parameter_key_required
 def stat():
-    select_from = request.args.get('from')
-    select_to = request.args.get('to')
+    logger = get_logger()
+    try:
+        select_from = request.args.get('from')
+        select_to = request.args.get('to')
 
-    table = db.Table('signal')
-    res: Sequence[Row[Signal]] = db.engine.connect().execute(table.select().filter(
-        # Signal.timestamp >= select_from
-        Signal.timestamp.between(select_from, select_to)
-    )).fetchall()
+        difference_in_hours = get_time_difference_in_hours()
+        difference_in_hours_db = get_db_time_difference_in_hours()
 
-    difference_in_hours = get_time_difference_in_hours()
-    difference_in_hours_db = get_db_time_difference_in_hours()
+        select_from_date = datetime.fromisoformat(select_from) + timedelta(hours=difference_in_hours_db)
+        select_to_date = datetime.fromisoformat(select_to) + timedelta(hours=difference_in_hours_db)
 
-    signals = [{"timestamp": row[1] - timedelta(hours=difference_in_hours_db), "at": row[2] - timedelta(hours=difference_in_hours_db)} for row in res]
+        table = db.Table('signal')
+        res: Sequence[Row[Signal]] = db.engine.connect().execute(table.select().filter(
+            Signal.timestamp.between(
+                select_from_date,
+                select_to_date
+            )
+        )).fetchall()
 
-    if len(signals) < 1:
-        return "<div>NO DATA. PLease use: <pre>/stat?from=2024-07-10&to=2024-07-30</pre></div>", 404
+        print(select_from_date, select_to_date)
 
-    if datetime.now() < datetime.fromisoformat(select_to):
-        future_date = datetime.now() - timedelta(hours=difference_in_hours)
-    else:
-        future_date = datetime.fromisoformat(select_to) - timedelta(hours=difference_in_hours)
 
-    img = plot(signals, datetime.fromisoformat(select_from), future_date)
-    return """
-        <div>
-            <img src='data:image/png;base64,{0}'>
-        </div>
-    """.format(img), 200
+
+        signals = [{"timestamp": row[1] - timedelta(hours=difference_in_hours_db), "at": row[2] - timedelta(hours=difference_in_hours_db)} for row in res]
+
+        if len(signals) < 1:
+            return "<div>NO DATA. PLease use: <pre>/stat?from=2024-07-10&to=2024-07-30</pre></div>", 404
+
+        if datetime.now() < datetime.fromisoformat(select_to):
+            future_date = datetime.now() - timedelta(hours=difference_in_hours)
+        else:
+            future_date = datetime.fromisoformat(select_to) - timedelta(hours=difference_in_hours)
+
+        img = plot(signals, datetime.fromisoformat(select_from), future_date)
+        return """
+            <div>
+                <img src='data:image/png;base64,{0}'>
+            </div>
+        """.format(img), 200
+    except Exception as e:
+        logger.error(f"HTTP /stat endpoint error: {e}")
+
+        return "Error", 502
 
 @app.route("/status")
 def status():
