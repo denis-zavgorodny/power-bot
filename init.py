3208ca8541a89b9e3bcac6e612d1b2b7aa1deec5
denys.zavhorodnii <denis.zavgorodny@gmail.com>
2024-07-21 18:14:38 +0300

    tune sqlite db

--- init.py
+++ init.py
@@ -7,4 +7,10 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///signals.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_size': 10,
+    'max_overflow': 20,
+    'pool_timeout': 30  # Optional: set pool timeout
+}
+
 db = SQLAlchemy(app)
