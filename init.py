db0e2a031f5a825cfe9615aaf88be23742cae1a9
denys.zavhorodnii <denis.zavgorodny@gmail.com>
2024-07-23 13:13:21 +0300

    Revert "increase pool size"
    This reverts commit c29832dab7246a03c5d6b3893bce350660358724.

--- init.py
+++ init.py
@@ -8,9 +8,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///signals.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-    'pool_size': 15,
-    'max_overflow': 30,
-    'pool_timeout': 60  # Optional: set pool timeout
+    'pool_size': 10,
+    'max_overflow': 20,
+    'pool_timeout': 30  # Optional: set pool timeout
 }
 
 db = SQLAlchemy(app)
