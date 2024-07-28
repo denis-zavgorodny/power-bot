e20492750fb94790f0022e05e35dd1f1a6058c9a
denys.zavhorodnii <denis.zavgorodny@gmail.com>
2024-07-28 21:16:58 +0300

    cover all zones

--- test/test_flask_endpoints.py
+++ test/test_flask_endpoints.py
@@ -10,7 +10,10 @@ from models.signal import Signal
 
 
 class TestApi(unittest.TestCase):
-    __current_mocked_datetime = datetime(2024, 7, 29, 3, 0, 0)
+    __dark_zone_datetime = datetime(2024, 7, 29, 3, 0, 0)
+    __grey_zone_datetime = datetime(2024, 7, 29, 6, 0, 0)
+    __white_zone_datetime = datetime(2024, 7, 29, 9, 0, 0)
+
     __last_signal_time = datetime(2024, 7, 29, 2, 0, 0)
 
     @classmethod
@@ -37,15 +40,15 @@ class TestApi(unittest.TestCase):
     @patch('main.datetime')
     @patch('yasno.power_state.datetime')
     @patch('yasno.api.datetime')
-    def test_status_endpoint(self, yasno_api_mock_datetime, yasno_power_mock_datetime, main_mock_datetime, mocked_request):
+    def test_status_endpoint_when_dark_zone(self, yasno_api_mock_datetime, yasno_power_mock_datetime, main_mock_datetime, mocked_request):
         # when
         with open(Path(__file__).parent / "__mocks__/http_calendar.json") as f:
             mocked_data = json.load(f)
 
         # when
-        main_mock_datetime.now.return_value = self.__current_mocked_datetime
-        yasno_power_mock_datetime.now.return_value = self.__current_mocked_datetime
-        yasno_api_mock_datetime.now.return_value = self.__current_mocked_datetime
+        main_mock_datetime.now.return_value = self.__dark_zone_datetime
+        yasno_power_mock_datetime.now.return_value = self.__dark_zone_datetime
+        yasno_api_mock_datetime.now.return_value = self.__dark_zone_datetime
 
         # when
         mocked_request.get.return_value.status_code = 200
@@ -58,6 +61,60 @@ class TestApi(unittest.TestCase):
         self.assertEqual({
             "has_electricity": False,
             "message": "Світло може повернутись в 05:00 якщо не буде застосовано світло-сірі зони"
+        } ,response.json)
+
+    @patch('yasno.api.requests')
+    @patch('main.datetime')
+    @patch('yasno.power_state.datetime')
+    @patch('yasno.api.datetime')
+    def test_status_endpoint_when_grey_zone(self, yasno_api_mock_datetime, yasno_power_mock_datetime, main_mock_datetime, mocked_request):
+        # when
+        with open(Path(__file__).parent / "__mocks__/http_calendar.json") as f:
+            mocked_data = json.load(f)
+
+        # when
+        main_mock_datetime.now.return_value = self.__grey_zone_datetime
+        yasno_power_mock_datetime.now.return_value = self.__grey_zone_datetime
+        yasno_api_mock_datetime.now.return_value = self.__grey_zone_datetime
+
+        # when
+        mocked_request.get.return_value.status_code = 200
+        mocked_request.get.return_value.json.return_value = mocked_data
+        response = self.client.get('/status')
+
+        # then
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("has_electricity", response.json)
+        self.assertEqual({
+            "has_electricity": False,
+            "message": "Світло має повернутись до 08:00. Зараз діє світло-сіра зона, світло можуть ввімкнути в будь-який момент"
+        } ,response.json)
+
+    @patch('yasno.api.requests')
+    @patch('main.datetime')
+    @patch('yasno.power_state.datetime')
+    @patch('yasno.api.datetime')
+    def test_status_endpoint_when_white_zone(self, yasno_api_mock_datetime, yasno_power_mock_datetime, main_mock_datetime, mocked_request):
+        # when
+        with open(Path(__file__).parent / "__mocks__/http_calendar.json") as f:
+            mocked_data = json.load(f)
+
+        # when
+        main_mock_datetime.now.return_value = self.__white_zone_datetime
+        yasno_power_mock_datetime.now.return_value = self.__white_zone_datetime
+        yasno_api_mock_datetime.now.return_value = self.__white_zone_datetime
+
+        # when
+        mocked_request.get.return_value.status_code = 200
+        mocked_request.get.return_value.json.return_value = mocked_data
+        response = self.client.get('/status')
+
+        # then
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("has_electricity", response.json)
+        self.assertEqual({
+            "has_electricity": False,
+            "message": "Планового відключення не мало б бути"
         } ,response.json)
 
 if __name__ == '__main__':
