6c760093212da1d2abf6f1325f434ef685ff72b4
denys.zavhorodnii <denis.zavgorodny@gmail.com>
2024-08-27 22:41:44 +0300

    detect day

--- yasno/power_state.py
+++ yasno/power_state.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Dict
 from timezone import timezone
 
@@ -36,7 +36,17 @@ class Power:
         if has_electricity is True and currentState is None:
             nextState = self.calendar.next_off()
             next_date = nextState.decoded(START).strftime("%Y-%m-%d %H:%M")
-            message = f"Наступне відключення: {next_date}"
+            next_time = nextState.decoded(START).strftime("%H:%M")
+            w = self.__is_today(nextState.decoded(START))
+            text_day = ""
+            if self.__is_today(nextState.decoded(START)):
+                text_day = f"сьогодні о {next_time}"
+            elif self.__is_tomorrow(nextState.decoded(END)):
+                text_day = f"завтра о {next_time}"
+            else:
+                text_day = next_date
+
+            message = f"Наступне відключення {text_day}"
         elif has_electricity is True and currentState is not None:
             next_date = currentState.decoded(END).strftime("%H:%M")
             message = f"Світло все ще можуть вимкнути до {next_date}"
@@ -51,6 +61,27 @@ class Power:
                 message = f"Світло має повернутись до {next_date}. Зараз діє світло-сіра зона, світло можуть ввімкнути в будь-який момент"
 
         return message
+
+    def __is_today(self, date: datetime) -> bool:
+        start = datetime.now(tz=timezone).replace(hour=0, minute=0, second=0, microsecond=0)
+        end = datetime.now(tz=timezone).replace(hour=23, minute=59, second=59, microsecond=999999)
+
+        try:
+            return end >= date >= start
+        except Exception as e:
+            self.logger.error(f"Day detection error: {e}")
+            return False
+
+
+    def __is_tomorrow(self, date: datetime) -> bool:
+        start = datetime.now(tz=timezone).replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
+        end = datetime.now(tz=timezone).replace(hour=23, minute=59, second=59, microsecond=0) + timedelta(days=1)
+
+        try:
+            return end >= date >= start
+        except Exception as e:
+            self.logger.error(f"Day detection error: {e}")
+            return False
 
 
     def __is_dark_zone(self, state: Dict) -> bool:
