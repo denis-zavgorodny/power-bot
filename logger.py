c238afcf83721f876724904d89258f33be67709f
denys-zavhorodnii <dzavhorodnii@kse.org.ua>
2025-10-22 10:40:45 +0300

    change level

--- logger.py
+++ logger.py
@@ -3,6 +3,7 @@ import logging
 logger = logging.getLogger("power-telegram-bot")
 
 logging.basicConfig(
+    level=logging.INFO,
     filename="power-bot.log",
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
